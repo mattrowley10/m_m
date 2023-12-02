731b32fd10db1ca5ddda0c42692b38b2c1188a7b
Matthew Rowley <mattrowley10@gmail.com>
2023-12-02 12:22:45 -0700

    redirect URI changed to or

--- src/API/script.js
+++ src/API/script.js
@@ -1,5 +1,7 @@
 const clientId = "30f051c5fe2146e5afeb4153911bdb99";
-const redirectUri = "http://localhost:5173/home";
+const redirectUri =
+  "http://localhost:5173/home" ||
+  "https://ornate-meerkat-7bb6c1.netlify.app/home";
 const url = "https://accounts.spotify.com/api/token";
 const scope =
   "user-read-playback-state user-modify-playback-state streaming user-read-private user-read-email";
